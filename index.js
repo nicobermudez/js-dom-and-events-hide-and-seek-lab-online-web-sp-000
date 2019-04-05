ca1445b054d30b645368ce60bc0b4482fb84f2e6
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-05 16:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll(".ranked-list")
 
-  for(let i = 0; i < rankedLists.length; i++) {
-    let children = rankedLists[i].children
+  
 
-    for(let j=0; j < children.length; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML ) + n
-    }
-  }
+  // for(let i = 0; i < rankedLists.length; i++) {
+  //   let children = rankedLists[i].children
+  //
+  //   for(let j=0; j < children.length; j++) {
+  //     children[j].innerHTML = parseInt(children[j].innerHTML ) + n
+  //   }
+  // }
 
 }
 
