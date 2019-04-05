05fcdefc733ed38636a0e787085d4e9725edfa29
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-05 16:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
     let children = rankedLists[i].children
 
     for(let j=0; j < children.length; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML + n)
+      children[j].innerHTML = parseInt(children[j].innerHTML ) + n
     }
   }
 
