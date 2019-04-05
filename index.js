67fec481361531cf8a8fcda63cbf1142c7aa6fea
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-05 16:56:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function increaseRankBy(n) {
 
 
 
-  // for(let i = 0; i < rankedLists.length; i++) {
-  //   let children = rankedLists[i].children
-  //
-  //   for(let j=0; j < children.length; j++) {
-  //     children[j].innerHTML = parseInt(children[j].innerHTML ) + n
-  //   }
-  // }
+  for(let i = 0; i < rankedLists.length; i++) {
+    let children = rankedLists[i].children
+
+    for(let j=0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML ) + n
+    }
+  }
 
 }
 
